778b9b3ba6ecaf1bf7cea9989c03b0787aefc915
Jose Luis Romero <joseluis.romero@babel.es>
2016-03-28 14:03:54 +0200

    Filter categories upgraded

--- dist/app.js
+++ dist/app.js
@@ -36347,13 +36347,21 @@ angular.module('moviedb').controller("MenuController", ["$scope", "$location", "
     	console.log(text);
         return moment(text).fromNow();
     }
-}]);;angular.module("moviedb").filter("categories", [function() {
-    return function(array) {
-        if (array != undefined)
-            return array.join(",");
-    }
-}]);
-;angular.module("moviedb").filter("schoolrating", [function() {
+}]);;angular.module("moviedb").filter("categories",
+    ["$log", function($log){
+        return function(arr, sep){
+            var items = arr || null;
+            var separator = sep || ", ";
+            if (items == null)
+                return "";
+            if (typeof arr.join === "undefined") {
+                $log.error("The value passed to the filter 'categories' must be an array.")
+                return "";
+            }
+            return arr.join(separator);
+        };
+    }]
+);;angular.module("moviedb").filter("schoolrating", [function() {
     return function(rating, mode) {
         if (rating <= 10 && 9 <= rating) {
             return (mode == "US") ? "A" : "SB";
--- scripts/filters/categories.js
+++ scripts/filters/categories.js
@@ -1,6 +1,15 @@
-angular.module("moviedb").filter("categories", [function() {
-    return function(array) {
-        if (array != undefined)
-            return array.join(",");
-    }
-}]);
+angular.module("moviedb").filter("categories",
+    ["$log", function($log){
+        return function(arr, sep){
+            var items = arr || null;
+            var separator = sep || ", ";
+            if (items == null)
+                return "";
+            if (typeof arr.join === "undefined") {
+                $log.error("The value passed to the filter 'categories' must be an array.")
+                return "";
+            }
+            return arr.join(separator);
+        };
+    }]
+);
