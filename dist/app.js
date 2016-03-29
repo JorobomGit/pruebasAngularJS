0fc20385b87ab6736da60a10e6d9428700f80b43
Jose Luis Romero <joseluis.romero@babel.es>
2016-03-29 12:07:14 +0200

    Form finished with badwords

--- dist/app.js
+++ dist/app.js
@@ -36315,7 +36315,46 @@ angular.module('moviedb').controller("MenuController", ["$scope", "$location", "
             }
         );
     }]
-);;angular.module("moviedb").directive("mediaItem", function () {
+);;angular.module("moviedb").directive('badwords', function() {
+    // Runs during compile
+    return {
+        // name: '',
+        // priority: 1,
+        // terminal: true,
+        // scope: {}, // {} = isolate, true = child, false/undefined = no change
+        // controller: function($scope, $element, $attrs, $transclude) {},
+        require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
+        // restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
+        // template: '',
+        // templateUrl: '',
+        // replace: true,
+        // transclude: true,
+        // compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+        link: function($scope, elem, attrs, cntrl) {
+            var badwords = ["fuck", "shit"];
+            console.log("badwords link", arguments);
+            console.log("0000:", $scope.movieForm.overview.modelValue);
+            // console.log($scope);
+            // console.log(elem);
+            // console.log(attrs);
+            // console.log(cntrl);
+            cntrl.$validators.badwords = function(modelValue, viewValue) {
+                var rawWords = modelValue || "";
+                var words = rawWords.split(" ");
+                for (var i in badwords) {
+                    if (words.indexOf(badwords[i]) >= 0) {
+                    	cntrl.badword = badwords[i];
+                        return false;
+                    }
+                }
+                cntrl.badword = "";
+                return true;
+            }
+
+        }
+    };
+});
+;angular.module("moviedb").directive("mediaItem", function () {
 	return {
 	 	restrict: "AE",
 	 	templateUrl: "views/mediaItem.html",
